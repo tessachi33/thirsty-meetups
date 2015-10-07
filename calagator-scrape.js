eeaedd57f49a8a01cb60bc7fc5138555059b34a7
momo-ozawa <mozawa92@gmail.com>
2015-10-07 16:26:59 -0700

    Add link, startTime, endTime to json

--- calagator-scrape.js
+++ calagator-scrape.js
@@ -2,6 +2,7 @@ var request = require('request');  // A simple way to make http calls
 var cheerio = require('cheerio');  // Provides jQuery manipulation on the backend
 var fs = require('fs');            // Gives us access to our computer’s file system
 var events = [];
+var links = [];
 
 request('http://www.calagator.org', function(err, res, body) {
   if (!err && res.statusCode === 200) {
@@ -14,18 +15,22 @@ request('http://www.calagator.org', function(err, res, body) {
 
       var info = {
         name: '',
-        time: '',
+        link: '',
+        startTime: '',
+        endTime: '',
         location: ''
       }
 
       info.name = eventData.children(':nth-child(1)').text();
-      info.time = eventData.children(':nth-child(3)').find('time').text();
+      info.link = eventData.children(':nth-child(1)').attr('href');
+      info.startTime = eventData.children(':nth-child(3)').children(':nth-child(1)').attr('datetime');
+      info.endTime = eventData.children(':nth-child(3)').children(':nth-child(2)').attr('datetime');
       info.location = eventData.children(':nth-child(3)').find('a').text();
 
       events.push(info);
     })
 
-    // The fs library helps us to write a file to our sysstem
+    // The fs library helps us to write a file to our system
     fs.writeFile('output.json', JSON.stringify(events, null, 4), function(err) {
       console.log('File successfully written! - Check your project directory for the output.json file');
     });
